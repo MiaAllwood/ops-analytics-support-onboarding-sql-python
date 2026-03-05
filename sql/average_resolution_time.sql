880785b9cef675e544629955b614c7ab34851e98
Mia <ameliaallwood@gmail.com>
2026-03-05 12:18:34 +0000

    Update average_resolution_time.sql

--- sql/average_resolution_time.sql
+++ sql/average_resolution_time.sql
@@ -1,1 +1,9 @@
+-- Average resolution time by issue type
 
+SELECT
+    issue_type,
+    COUNT(ticket_id) AS total_tickets,
+    AVG(resolution_time_hours) AS avg_resolution_hours
+FROM support_tickets
+GROUP BY issue_type
+ORDER BY avg_resolution_hours DESC;
